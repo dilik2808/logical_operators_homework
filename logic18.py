170dd15003c53f1da6b97b2652b228fdfbe09760
Dilshod <dilik2808@gmail.com>
2022-11-07 16:51:50 +0500

    18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,29 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1= a%10
+    x2= a%100//10
+    x3= a%1000//100
+    x4= a%10000//1000
+    x5= a//10000
+    
+    if x1>x2:
+        if x1>x3:
+            if x1>x4:
+                if x1>x5:
+                    return True
+                else: return  False
+    if x2>x3:
+        if x2>x4:
+            if x2>x5:
+                return True
+            else: return False
+    if x3>x4:
+        if x3>x5:
+            return True
+        else: return False
+    if x4>x5:
+        return True
+    else: return False
+
+print(main (12385))
