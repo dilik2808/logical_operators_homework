eb5436176472bab9d867a8a83412949fd15d3913
Dilshod <dilik2808@gmail.com>
2022-11-07 16:39:10 +0500

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,29 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1= a%10
+    x2= a%100//10
+    x3= a%1000//100
+    x4= a%10000//1000
+    x5= a//10000
+    if x5>x4:
+        if x5>x3:
+            if x5>x2:
+                if x5>x1:
+                    return True
+                else: return  False
+    if x4>x3:
+        if x4>x2:
+            if x4>x1:
+                return True
+            else: return False
+    if x3>x2:
+        if x3>x1:
+            return True
+        else: return False
+    if x2>x1:
+        return True
+    else: return False
+
+
+print (main(54321))
